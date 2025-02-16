a6d23ab4f9df0fb56670135c4aa457d7fd103ee1
ojefersoncode <jefersonvidal999@gmail.com>
2025-02-16 16:39:38 -0300

    Add animate background  to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import { ShimmerButton } from "@/components/magicui/shimmer-button";
 import { ShinyButton } from "@/components/magicui/shiny-button";
 import { HeroVideoDialogDemo } from "@/components/HeroVideo";
+import { Meteors } from "@/components/magicui/meteors";
 
 const Page = () => {
   const router = useRouter();
@@ -19,11 +20,9 @@ const Page = () => {
   return (
     <>
       <div className="min-h-screen w-full bg-indigo-950 text-white flex flex-col items-center justify-center px-6 text-center">
+      <Meteors />
         <header className="w-full mx-4 flex justify-between items-center py-4">
-          <h1 className="text-2xl font-bold flex items-center">
-            <span className="text-indigo-400">Lep</span>{" "}
-            <span className="">trum</span>
-          </h1>
+          <h1 className="text-2xl font-bold flex items-center">Leptrum</h1>
           <nav className="hidden md:flex space-x-6 font-semibold">
             <a href="#" className="hover:text-indigo-400">
               Início
@@ -41,30 +40,30 @@ const Page = () => {
             </RippleButton>
           </div>
         </header>
-        <main className="flex-1 flex flex-col mt-20 mb-16 items-center justify-center">
-          <h1 className="text-3xl md:text-5xl max-md:text-start font-bold text-center">
-            Construa seu catálogo com poucos cliques.
+        <main className="flex-1 flex flex-col mt-10 items-center justify-center">
+          <h1 className="text-3xl font-bold text-center">
+            Organize seus produtos online e aumente suas vendas em poucos dias.
           </h1>
-          <p className="flex items-center justify-center text-lg max-md:text-start text-center font-sans max-w-3xl mt-4">
-            A Leptrum é a plataforma ideal para organizar produtos com
-            eficiência. Conte com nossa dashboard avançada e análises de IA para
-            otimizar estratégias e crescer seu negócio. 🚀
+          <p className="flex items-center justify-center text-base text-center font-sans max-w-3xl mt-4">
+            Com a Leptrum, você cria seu catálogo online, gerencia tudo com
+            facilidade e utiliza o poder da AI para levar seu negócio a um novo
+            nível. 🚀
           </p>
-          <div className="flex flex-wrap items-center justify-center mt-6 gap-4">
-            <ShimmerButton 
-            onClick={handleResgisterClick}
-            className="shadow-2xl">
-              <span className="whitespace-pre-wrap text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
-                Começe Gratuitamente
+          <div className="flex items-center justify-start mt-6 gap-4">
+            <ShimmerButton
+              onClick={handleResgisterClick}
+              className="shadow-2xl"
+            >
+              <span className="whitespace-pre-wrap text-sm leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10">
+                COMEÇAR AGORA
               </span>
             </ShimmerButton>
 
-            <ShinyButton
-            onClick={handleLoginClick}
-            >Acessar Minha Conta</ShinyButton>
+            <ShinyButton 
+            onClick={handleLoginClick}>Acessar conta</ShinyButton>
           </div>
 
-          <div className="flex justify-center mt-10 items-center">
+          <div className="flex justify-center mt-7 items-center">
             <HeroVideoDialogDemo />
           </div>
         </main>
