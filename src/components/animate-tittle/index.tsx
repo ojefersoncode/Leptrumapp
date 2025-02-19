09607ac17d9e80815918d9ab8a02808ce8b93a53
ojefersoncode <jefersonvidal999@gmail.com>
2025-02-19 02:49:32 -0300

    Att tittle size and effects

--- src/components/animate-tittle/index.tsx
+++ src/components/animate-tittle/index.tsx
@@ -3,7 +3,7 @@ import { SparklesText } from "@/components/magicui/sparkles-text";
 export function AnimatedTittle() {
   return (
     <SparklesText
-      className="text-5xl max-sm:text-3xl"
+      className="text-5xl max-sm:text-3xl justify-center items-center"
       text="Ficou facil organizar seus produtos online, com apenas alguns cliques."
     />
   );
@@ -13,7 +13,7 @@ export function AnimatedTittle() {
 export function AnimatedTittleSection2() {
   return (
     <SparklesText
-      className="text-4xl max-sm:text-2xl max-w-xl"
+      className="text-4xl max-sm:text-2xl max-w-xl justify-center items-center"
       text="Conecte todos seus apps em apenas um serviço"
     />
   );
