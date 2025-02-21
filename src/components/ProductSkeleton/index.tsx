e51a0ee786ead495a73d3847c64f2aa29f767e03
ojefersoncode <jefersonvidal999@gmail.com>
2025-02-21 15:36:41 -0300

    Edit component product skeleton

--- src/components/ProductSkeleton/index.tsx
+++ src/components/ProductSkeleton/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 
 const ProductSkeleton = () => (
-    <div className="flex w-full flex-col space-y-3">
+    <div className="flex w-full flex-col space-y-3 m-2">
         <Skeleton className="h-[200px] w-[290px] max-md:w-[180px] rounded-xl" />
         <div className="space-y-3">
             <div className="flex gap-4">
