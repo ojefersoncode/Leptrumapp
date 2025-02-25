163fe5e2bc59699e05afa466592b78a612e03f69
ojefersoncode <jefersonvidal999@gmail.com>
2025-02-25 18:25:19 -0300

    Edit skeleton

--- src/components/ProductSkeleton/index.tsx
+++ src/components/ProductSkeleton/index.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 
 const ProductSkeleton = () => (
-  <div className="flex w-full flex-col space-y-3 m-2">
-    <Skeleton className="h-[200px] m-4 w-[290px] max-md:w-[180px] rounded-xl" />
+  <div className="flex w-full flex-col bg-white space-y-3 m-2">
+    <Skeleton className="h-[200px] w-[290px] max-md:w-[170px] rounded-xl" />
     <div className="mx-4">
       <div className="space-y-3">
         <div className="flex gap-2">
           <Skeleton className="h-3 w-[20px] max-md:w-[20px]" />
-          <Skeleton className="h-3 w-[120px] max-md:w-[100px] " />
+          <Skeleton className="h-3 w-[120px] max-md:w-[90px] " />
         </div>
 
         <div className="flex gap-4 my-1">
-          <Skeleton className="h-3 w-[290px] max-md:w-[190px]" />
+          <Skeleton className="h-3 w-[290px] max-md:w-[180px]" />
         </div>
 
         <div className="flex w-full justify-between items-center mt-2">
