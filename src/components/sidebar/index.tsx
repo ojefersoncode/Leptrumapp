175c4bd833217d5e8221bf7ae3fd194dce14bfc2
ojefersoncode <jefersonvidal999@gmail.com>
2025-02-25 19:22:46 -0300

    Edit search filter

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useState } from "react";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Dialog } from "@radix-ui/react-dialog";
@@ -25,6 +24,7 @@ import {
   DialogHeader,
 } from "@/components/ui/dialog";
 import { Input } from "../ui/input";
+import { useState } from "react";
 
 export function Sidebar() {
   const [showFilters, setShowFilters] = useState(false);
@@ -32,10 +32,7 @@ export function Sidebar() {
   return (
     <div className="flex flex-col w-full bg-gray-800 bg-opacity-90 max-md:p-2">
       <div className="flex w-full flex-col sm:gap-4 sm:py-4">
-        <header
-          className="sticky top-0 z-30 flex px-2 items-center 
-                 gap-4 sm:static sm:h-auto sm:border-0"
-        >
+        <header className="sticky top-0 z-30 flex px-2 items-center gap-4 sm:static sm:h-auto sm:border-0">
           <div className="flex w-full items-center bg-transparent gap-3">
             <Sheet>
               <SheetTrigger asChild>
@@ -44,7 +41,6 @@ export function Sidebar() {
                   <span className="sr-only">Menu</span>
                 </button>
               </SheetTrigger>
-
               <SheetContent
                 side="left"
                 className="sm:max-w-x text-slate-50 outline-none border-none bg-gray-800 flex flex-col justify-between h-full"
@@ -53,73 +49,62 @@ export function Sidebar() {
                   <DialogTitle>
                     <VisuallyHidden>Leptrum</VisuallyHidden>
                   </DialogTitle>
-
                   <nav className="grid gap-5 text-lg font-medium">
                     <div className="flex items-center mx-2">
-                      <Link
-                        href="#"
-                        className="flex text-lg items-center text-indigo-100"
-                      >
-                        <span className="sr-only">Logo</span>
-                      </Link>
                       <h1 className="text-indigo-50">Menu</h1>
                     </div>
-
                     <div>
                       <div className="flex items-center text-red-600 px-2.5 mb-2">
                         <h1 className="font-bold text-lg">Leptrum store</h1>
                       </div>
-
-                      <div className="flex items-center px-2.5 text-indigo-50 gap-1">
+                      <div className="flex items-center px-2.5 text-indigo-50 gap-1 whitespace-nowrap flex-shrink-0">
                         <MapPin className="size-4" />
-                        <span className="text-sm">Ponte Nova</span>
+                        <span className="font-poppins text-sm">Ponte Nova</span>
                       </div>
                     </div>
                     <hr className="bg-slate-600 opacity-50" />
-
                     <Link
                       href="#"
                       className="flex items-center gap-4 px-2.5 text-slate-50 hover:text-red-600"
                     >
-                      <Home className="h-5 w-5" />
+                      <Home className="h-5 w-5 transition-all" />
                       Home
                     </Link>
                     <Link
                       href="#"
                       className="flex items-center gap-4 px-2.5 text-slate-50 hover:text-red-600"
                     >
-                      <Store className="h-5 w-5" />
+                      <Store className="h-5 w-5 transition-all" />
                       Lojas
                     </Link>
                     <Link
                       href="#"
                       className="flex items-center gap-4 px-2.5 text-slate-50 hover:text-red-600"
                     >
-                      <Heart className="h-5 w-5" />
+                      <Heart className="h-5 w-5 transition-all" />
                       Salvos
                     </Link>
                     <Link
                       href="/dashboard/${userId}"
                       className="flex items-center gap-4 px-2.5 text-slate-50 hover:text-red-600"
                     >
-                      <ChartLine className="h-5 w-5" />
+                      <ChartLine className="h-5 w-5 transition-all" />
                       Dashboard
                     </Link>
                     <Link
                       href="#"
                       className="flex items-center gap-4 px-2.5 text-slate-50 hover:text-red-600"
                     >
-                      <Settings className="h-5 w-5" />
+                      <Settings className="h-5 w-5 transition-all" />
                       Configurações
                     </Link>
                   </nav>
                 </div>
-
                 <Link
                   href="#"
                   className="flex items-center font-bold text-xl gap-4 px-2.5 text-slate-50 hover:text-red-600 mt-auto"
                 >
-                  <LogOut className="h-5 w-5" />
+                  <LogOut className="h-5 w-5 transition-all" />
                   Sair
                 </Link>
               </SheetContent>
@@ -137,7 +122,6 @@ export function Sidebar() {
           </div>
 
           <div className="flex w-full items-center justify-end gap-2">
-            {/* Dialog search */}
             <Dialog>
               <DialogTrigger asChild>
                 <Button className="bg-transparent hover:bg-transparent text-white hover:text-red-600 px-1">
@@ -154,47 +138,54 @@ export function Sidebar() {
                   <div className="flex gap-4">
                     <Input
                       type="search"
-                      placeholder="Buscar..."
-                      className="w-full px-4 py-2 rounded text-black font-semibold bg-gray-100 focus:outline-none"
+                      placeholder="buscar..."
+                      className="w-full px-4 py-2 rounded text-black font-semibold bg-gray-100 border border-none focus:outline-none"
                     />
                     <Button className="bg-gray-800 hover:bg-gray-700">
                       <Search className="size-7" />
                     </Button>
                   </div>
 
-                  <div className="flex w-full items-center justify-between mx-auto">
-                    <h1 className="font-bold text-lg text-white">
-                      Filtro de busca
-                    </h1>
-                    <Button
-                      onClick={() => setShowFilters(!showFilters)}
-                      className="hover:bg-gray-800"
-                    >
-                      <ListFilter className="size-7 text-white" />
-                    </Button>
-                  </div>
-
-                  {showFilters && (
-                    <div className="flex flex-wrap gap-4 transition-all duration-300">
-                      <span className="text-sm rounded bg-red-600 hover:bg-red-700 px-2 py-1 text-white cursor-pointer">
-                        Usado
-                      </span>
-                      <span className="text-sm rounded bg-red-600 hover:bg-red-700 px-2 py-1 text-white cursor-pointer">
-                        Novo
-                      </span>
-                      <span className="text-sm rounded bg-red-600 hover:bg-red-700 px-2 py-1 text-white cursor-pointer">
-                        Promoção
-                      </span>
+                  <div className="text-black rounded">
+                    <div className="flex justify-between mx-auto">
+                      <h1 className="font-bold text-lg text-white">
+                        Filtro de busca
+                      </h1>
+                      <Button
+                        className="transition-all fade-in-15 hover:bg-gray-800"
+                        onClick={() => setShowFilters(!showFilters)}
+                      >
+                        <ListFilter className="size-7 text-white" />
+                      </Button>
                     </div>
-                  )}
-
-                  <hr className="w-full text-gray-900" />
-
+                    {showFilters && (
+                      <div className="flex flex-wrap gap-4">
+                        <a className="flex flex-nowrap text-sm rounded text-indigo-50 bg-red-600 hover:bg-red-700 px-2 py-1">
+                          Usado
+                        </a>
+                        <a className="flex flex-nowrap text-sm rounded text-indigo-50 bg-red-600 hover:bg-red-700 px-2 py-1">
+                          Novo
+                        </a>
+                        <a className="flex flex-nowrap text-sm rounded text-indigo-50 bg-red-600 hover:bg-red-700 px-2 py-1">
+                          Promoção
+                        </a>
+                      </div>
+                    )}
+                  </div>
+                  <hr className="w-full my-1 text-gray-900" />
                   <div className="text-white">
-                    <span className="mb-1 block">Sugestões de busca...</span>
-                    <span className="mb-1 block">Sugestões de busca...</span>
-                    <span className="mb-1 block">Sugestões de busca...</span>
-                    <span className="mb-1 block">Sugestões de busca...</span>
+                    <div className="mb-1">
+                      <span>Sugestões de busca...</span>
+                    </div>
+                    <div className="mb-1">
+                      <span>Sugestões de busca...</span>
+                    </div>
+                    <div className="mb-1">
+                      <span>Sugestões de busca...</span>
+                    </div>
+                    <div className="mb-1">
+                      <span>Sugestões de busca...</span>
+                    </div>
                   </div>
                 </div>
               </DialogContent>
