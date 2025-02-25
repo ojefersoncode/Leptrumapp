339b23e9368a80e37685c4bf5d9f6a3b542b4101
ojefersoncode <jefersonvidal999@gmail.com>
2025-02-25 18:54:48 -0300

    Edit skeleton responsividade and sizes

--- src/components/ProductSkeleton/index.tsx
+++ src/components/ProductSkeleton/index.tsx
@@ -3,16 +3,16 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const ProductSkeleton = () => (
   <div className="flex w-full flex-col items-center justify-center space-y-3">
-    <Skeleton className="h-[200px] w-[290px] max-md:w-[180px] rounded-md" />
-    <div className="w-full px-4 space-y-3">
+    <Skeleton className="h-[200px] w-[290px] max-md:w-[190px] rounded-md" />
+    <div className="w-full px-2 space-y-3">
       <div className="flex gap-2">
         <Skeleton className="h-3 w-[20px] max-md:w-[20px]" />
         <Skeleton className="h-3 w-[120px] max-md:w-[100px]" />
       </div>
       <Skeleton className="h-3 w-[290px] max-md:w-[170px] my-1" />
-      <div className="flex w-full justify-between items-center gap-4 mt-2">
+      <div className="flex w-full justify-between items-center gap-7 mt-2">
         <Skeleton className="h-3 w-[120px] max-md:w-[160px]" />
-        <Skeleton className="h-6 w-[30px] max-md:w-[30px] mx-5" />
+        <Skeleton className="h-6 w-[40px] max-md:w-[40px] ml-10" />
       </div>
     </div>
   </div>
