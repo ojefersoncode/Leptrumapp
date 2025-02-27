cf455329fdd39326843a344b55c3e058547d20c8
ojefersoncode <jefersonvidal999@gmail.com>
2025-02-27 18:57:02 -0300

    Edit heade mobile

--- src/components/HeaderMobile/index.tsx
+++ src/components/HeaderMobile/index.tsx
@@ -38,8 +38,8 @@ export function HeaderMobile() {
         <header className="flex px-2 items-center w-full justify-between gap-4 sm:static sm:h-auto sm:border-0">
           <div className="flex w-full items-center bg-transparent gap-3">
             <div className="flex w-fulljustify-start items-center">
-              <div className="flex w-full items-center justify-center gap-0.5 text-base font-bold">
-                <img className="size-5" src="/logo/Leptrum.png" alt="logo" />
+              <div className="flex w-full items-center justify-center gap-0.5 text-lg font-bold">
+                <img className="size-6" src="/logo/Leptrum.png" alt="logo" />
                 <div className="flex select-none">
                   <h1 className="text-red-500">Lep</h1>
                   <h1 className="text-red-50">trum</h1>
@@ -49,7 +49,7 @@ export function HeaderMobile() {
           </div>
 
           <button className="flex items-center gap-2 bg-transparent hover:bg-transparent text-white hover:text-red-600 px-1">
-            <span className="text-xs">UserName</span>
+            <span className="text-sm">UserName</span>
             <CircleUser className="size-5 text-white hover:text-red-600" />
           </button>
         </header>
