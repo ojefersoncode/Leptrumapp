0706d8a5ea94278aade709e60b927ed5cfa8e64d
ojefersoncode <jefersonvidal999@gmail.com>
2025-02-27 19:43:34 -0300

    Edit button top right

--- src/components/ButtonTop/index.tsx
+++ src/components/ButtonTop/index.tsx
@@ -6,7 +6,7 @@ export default function ButtonTop() {
   };
 
   return (
-    <div className="fixed bottom-5 max-md:bottom-16 right-5">
+    <div className="fixed bottom-5 max-md:bottom-16 right-4 max-md:right-2">
       <button
         onClick={scrollToTop}
         className="relative p-3 bg-gray-900 text-white rounded-full shadow-lg hover:bg-red-500 transition"
