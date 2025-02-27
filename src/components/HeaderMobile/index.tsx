b570039b57a44b32c350211fe85ba88630b572a3
ojefersoncode <jefersonvidal999@gmail.com>
2025-02-27 20:54:04 -0300

    Edit Header mobile

--- src/components/HeaderMobile/index.tsx
+++ src/components/HeaderMobile/index.tsx
@@ -35,7 +35,7 @@ export function HeaderMobile() {
   return (
     <div className="flex flex-col w-full md:hidden bg-gray-800 max-md:p-2">
       <div className="flex w-full flex-col sm:gap-4 py-1">
-        <header className="flex px-2 items-center w-full justify-between gap-4 sm:static sm:h-auto sm:border-0">
+        <header className="flex px-1 items-center w-full justify-between gap-4 sm:static sm:h-auto sm:border-0">
           <div className="flex w-full items-center bg-transparent gap-3">
             <div className="flex w-fulljustify-start items-center">
               <div className="flex w-full items-center justify-center gap-0.5 text-lg font-bold">
