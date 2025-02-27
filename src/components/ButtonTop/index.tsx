a9853a3e248538824d9757c8055eea91c6ae68d4
ojefersoncode <jefersonvidal999@gmail.com>
2025-02-27 19:04:35 -0300

    Animated button top

--- src/components/ButtonTop/index.tsx
+++ src/components/ButtonTop/index.tsx
@@ -9,10 +9,33 @@ export default function ButtonTop() {
     <div className="fixed bottom-5 max-md:bottom-16 right-5">
       <button
         onClick={scrollToTop}
-        className="p-3 bg-red-500 text-white rounded-full border border-black shadow-lg hover:bg-red-400 transition"
+        className="relative p-3 bg-red-500 text-white rounded-full shadow-lg hover:bg-red-400 transition"
       >
-        <ArrowUp size={14} />
+        <ArrowUp className="size-7" />
+        <span className="absolute inset-0 rounded-full border-2 border-transparent animate-spin-border"></span>
       </button>
+      <style jsx>{`
+        @keyframes spin-border {
+          0% {
+            border-color: white transparent transparent transparent;
+          }
+          25% {
+            border-color: transparent white transparent transparent;
+          }
+          50% {
+            border-color: transparent transparent white transparent;
+          }
+          75% {
+            border-color: transparent transparent transparent white;
+          }
+          100% {
+            border-color: white transparent transparent transparent;
+          }
+        }
+        .animate-spin-border {
+          animation: spin-border 1.5s linear infinite;
+        }
+      `}</style>
     </div>
   );
 }
