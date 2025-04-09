69a96342e6c961201c1826b6300eabb2cf50cbd1
Ojefersoncode <jefersonvidal999@gmail.com>
2025-04-09 15:57:54 -0300

    Fix error

--- components/landing/About.tsx
+++ components/landing/About.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { Statistics } from '../../components/landing/Statistics';
-
 export const About = () => {
   return (
     <section id="about" className="w-full py-14 sm:py-20">
@@ -19,22 +17,20 @@ export const About = () => {
                 Os templates
               </h2>
               <p className="max-w-4xl text-base md:text-xl text-muted-foreground mt-4 leading-relaxed">
-                Nossos templates são, desenvolvidos com{' '}
-                <strong>Next.js</strong> e <strong>Supabase </strong> , oferecem
-                uma solução moderna, eficiente e escalável para startups e
-                negócios digitais. Com <strong>Next.js</strong>, garantimos
-                carregamento rápido, renderização otimizada e uma experiência de
-                usuário fluida. Já o <strong>Supabase </strong> fornece
-                autenticação segura, banco de dados escalável e APIs poderosas,
-                eliminando a complexidade do backend. Além disso, nosso código é
-                <strong>limpo </strong> e <strong>modular </strong>, facilitando
-                personalizações e manutenção. Ao utilizar nossos templates, você
-                reduz drasticamente o tempo de desenvolvimento e pode focar no
+                Nossos templates são, desenvolvidos com <strong>Next.js</strong>{' '}
+                e <strong>Supabase </strong> , oferecem uma solução moderna,
+                eficiente e escalável para startups e negócios digitais. Com{' '}
+                <strong>Next.js</strong>, garantimos carregamento rápido,
+                renderização otimizada e uma experiência de usuário fluida. Já o{' '}
+                <strong>Supabase </strong> fornece autenticação segura, banco de
+                dados escalável e APIs poderosas, eliminando a complexidade do
+                backend. Além disso, nosso código é<strong>limpo </strong> e{' '}
+                <strong>modular </strong>, facilitando personalizações e
+                manutenção. Ao utilizar nossos templates, você reduz
+                drasticamente o tempo de desenvolvimento e pode focar no
                 crescimento do seu negócio com tecnologia de ponta.
               </p>
             </div>
-
-            <Statistics />
           </div>
         </div>
       </div>
