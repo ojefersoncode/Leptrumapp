7500c9bcf75c59b44b89e5425b07361cc9e7e3da
Ojefersoncode <jefersonvidal999@gmail.com>
2025-04-09 16:06:20 -0300

    Fix error landing

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,12 @@ import { Hero } from '../components/landing/Hero';
 import { Navbar } from '../components/landing/Navbar';
 import { Newsletter } from '../components/landing/Newsletter';
 import { ScrollToTop } from '../components/landing/ScrollToTop';
-import { createClient } from '../utils/supabase/server';
 
 export default async function LandingPage() {
-  const supabase = await createClient();
-
-  const {
-    data: { user }
-  } = await supabase.auth.getUser();
-
   return (
     <>
       <div className="w-full overflow-hidden">
-        <Navbar user={user} />
+        <Navbar user={null} />
         <Hero />
         <Vitrine />
         <Newsletter />
