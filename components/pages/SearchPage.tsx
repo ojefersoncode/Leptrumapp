d5ca2220ac61d93da169be52987bc1793cf2f961
Ojefersoncode <jefersonvidal999@gmail.com>
2025-04-10 17:19:56 -0300

    Fix navbar error rm dash

--- components/pages/SearchPage.tsx
+++ components/pages/SearchPage.tsx
@@ -6,6 +6,7 @@ import { ProductCard } from '../SearchComponents/ProductCard';
 import { products as allProducts } from '../SearchComponents/mockProducts';
 import Navbar from '../HomeComponents/Navbar';
 import { Footer } from '../landing/Footer';
+import { Truck } from 'lucide-react';
 
 export function SearchPage() {
   const [filters, setFilters] = useState({
@@ -25,6 +26,12 @@ export function SearchPage() {
 
   return (
     <div className="w-full mx-auto bg-slate-200 h-screen">
+      <div className="flex justify-center items-center w-full bg-red-700 hover:bg-red-600 transition-all duration-200 p-3">
+        <div className="flex items-center text-sm max-sm:text-xs font-semibold ">
+          <Truck className="size-5 max-sm:size-4 mr-2" /> Frete grátis para
+          minas gerais
+        </div>
+      </div>
       <Navbar />
       <div className="p-7 bg-slate-200">
         <div className="pt-5">
