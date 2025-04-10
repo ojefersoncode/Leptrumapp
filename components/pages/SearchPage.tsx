e2d6c0104424e87a0f65bbc7c58ea8e05b68ccb6
Jeferson  Code <134572153+ojefersoncode@users.noreply.github.com>
2025-04-10 17:48:16 -0300

    Update SearchPage.tsx

--- components/pages/SearchPage.tsx
+++ components/pages/SearchPage.tsx
@@ -25,7 +25,7 @@ export function SearchPage() {
   });
 
   return (
-    <div className="w-full mx-auto bg-slate-200 h-screen">
+    <div className="w-full mx-auto bg-slate-200">
       <div className="flex justify-center items-center w-full bg-red-700 hover:bg-red-600 transition-all duration-200 p-3">
         <div className="flex items-center text-sm max-sm:text-xs font-semibold ">
           <Truck className="size-5 max-sm:size-4 mr-2" /> Frete grátis para
