483178c497f4ab0bb65f0a4d4caab0151ab6f223
Ojefersoncode <jefersonvidal999@gmail.com>
2025-04-18 17:39:36 -0300

    Edit Products

--- components/HomeComponents/Products.tsx
+++ components/HomeComponents/Products.tsx
@@ -42,30 +42,29 @@ const Products = () => {
             {products.slice(0).map((product) => (
               <CardContent
                 key={product.id}
-                className="bg-white shadow-md p-2 flex flex-col justify-between h-[330px] 
-                max-sm:h-[340px]"
+                className="bg-white shadow-md p-2 flex flex-col justify-between h-[320px] max-sm:h-[330px]"
               >
                 <img
                   src={product.image}
                   alt={product.title}
-                  className="w-full h-44 object-contain py-2 p-4 transition-all hover:scale-110"
+                  className="w-full h-44 object-contain p-4 transition-all hover:scale-110"
                 />
                 <CardFooter className="flex flex-col justify-between flex-1 p-1">
-                  <div>
-                    <h1 className="w-full text-center text-sm line-clamp-2 text-slate-900">
+                  <div className="w-full text-center pb-1 max-sm:pb-2">
+                    <h1 className="text-sm line-clamp-2 text-slate-900">
                       {product.title}
                     </h1>
-                    <div className="flex w-full justify-center items-center gap-2 mt-2">
+                    <div className="flex w-full justify-center items-center gap-2 pt-2">
                       <h2 className="text-base font-bold text-red-600 max-sm:font-bold">
                         R$: {product.price.toFixed(2)}
                       </h2>
-                      <h2 className="text-red-600 font-semibold text-sm">
+                      <h2 className="text-red-600 font-semibold text-base">
                         no PIX
                       </h2>
                     </div>
                   </div>
 
-                  <div className="flex w-full justify-center items-center mx-4 max-sm:mx-6">
+                  <div className="flex w-full justify-center items-center mb-2 mx-4 max-sm:mx-6">
                     <button className="flex w-full justify-center items-center rounded-lg bg-red-600 hover:bg-red-500 text-white hover:text-white mt-2">
                       <span className="flex items-center gap-2 text-sm py-1.5">
                         Adicionar <ShoppingBag className="size-4" />
