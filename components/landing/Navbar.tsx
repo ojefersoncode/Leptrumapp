d9720688e4c62900fdbe7efa3685b95491c1ba3c
Ojefersoncode <jefersonvidal999@gmail.com>
2025-04-18 16:20:22 -0300

    Fix error Navbar component

--- components/landing/Navbar.tsx
+++ components/landing/Navbar.tsx
@@ -1,23 +1,22 @@
 'use client';
 
-import React from 'react';
-import { useState } from 'react';
+import * as React from 'react';
 import {
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuList
 } from '@/components/ui/navigation-menu';
-import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger
-} from '@/components/ui/sheet';
 
-import { Button } from '../../components/ui/button';
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger
+} from '@/components/ui/drawer';
+
+import { Button } from '@/components/ui/button';
 import { Menu } from 'lucide-react';
-import { useRouter } from 'next/navigation';
 
 interface RouteProps {
   href: string;
@@ -25,91 +24,73 @@ interface RouteProps {
 }
 
 const routeList: RouteProps[] = [
-  {
-    href: '/',
-    label: 'Inicio'
-  },
-  {
-    href: '/Home',
-    label: 'Loja'
-  },
-  {
-    href: '/#about',
-    label: 'Sobre'
-  }
+  { href: '/', label: 'Inicio' },
+  { href: '/Home', label: 'Loja' },
+  { href: '/#about', label: 'Sobre' }
 ];
 
 export const Navbar = () => {
-  const router = useRouter();
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = React.useState(false);
 
   return (
-    <header className="stick bg-slate-100 top-0 z-40 w-full ">
+    <header className="stick bg-slate-100 top-0 z-40 w-full">
       <NavigationMenu className="mx-auto">
-        <NavigationMenuList className="container py-2 px-3 md:px-6 w-screen flex justify-between ">
+        <NavigationMenuList className="container py-2 px-3 md:px-6 w-screen flex justify-between">
+          {/* Logo */}
           <NavigationMenuItem className="font-bold flex">
             <div className="flex gap-2 text-xl font-bold items-center">
-              <div className="flex">
-                <img
-                  className="w-20 select-none cursor-context-menu"
-                  src="Leptrum-logo.png"
-                  alt="logo"
-                />
-              </div>
+              <img
+                className="w-20 select-none cursor-context-menu"
+                src="Leptrum-logo.png"
+                alt="logo"
+              />
             </div>
           </NavigationMenuItem>
 
-          {/* mobile */}
+          {/* Mobile Drawer Trigger */}
           <span className="flex md:hidden bg-slate-100 text-slate-900">
-            <Sheet open={isOpen} onOpenChange={setIsOpen}>
-              <SheetTrigger
-                className="px-2 bg-slate-100 text-slate-900"
-                asChild
-              >
+            <Drawer open={isOpen} onOpenChange={setIsOpen}>
+              <DrawerTrigger asChild>
                 <Button variant="outline" size="icon" className="bg-slate-100">
                   <Menu className="h-4 w-4 text-slate-900" />
                 </Button>
-              </SheetTrigger>
+              </DrawerTrigger>
 
-              <SheetContent
-                className="p-0 bg-slate-100 text-slate-900 h-screen"
-                side={'right'}
-              >
-                <SheetHeader className="bg-slate-100 p-0">
-                  <SheetTitle className="font-bold text-xl select-none p-0">
+              <DrawerContent className="bg-slate-100 text-slate-900 px-4">
+                <DrawerHeader>
+                  <DrawerTitle className="flex max-sm:justify-center font-bold text-xl select-none">
                     <img
-                      className="w-24 select-none cursor-context-menu p-4"
+                      className="w-24 select-none cursor-context-menu"
                       src="logo.png"
                       alt="logo"
                     />
-                  </SheetTitle>
-                </SheetHeader>
-                <nav className="flex flex-col px-4 items-center text-base text-bold gap-4 pt-4 bg-slate-100 text-slate-950 h-screen">
-                  {routeList.map(({ href, label }: RouteProps) => (
+                  </DrawerTitle>
+                </DrawerHeader>
+                <nav className="flex sm:flex-col sm:items-start items-center justify-center text-base font-bold sm:gap-4 gap-7 max-sm:mb-7 pt-2">
+                  {routeList.map(({ href, label }) => (
                     <a
-                      rel="noreferrer noopener"
                       key={label}
                       href={href}
+                      className="hover:text-slate-600"
                       onClick={() => setIsOpen(false)}
                     >
                       {label}
                     </a>
                   ))}
                 </nav>
-              </SheetContent>
-            </Sheet>
+              </DrawerContent>
+            </Drawer>
           </span>
 
-          {/* desktop */}
+          {/* Desktop Menu */}
           <nav className="hidden md:flex gap-7">
-            {routeList.map((route: RouteProps, i) => (
+            {routeList.map(({ href, label }) => (
               <a
-                rel="noreferrer noopener"
-                href={route.href}
-                key={i}
-                className={`text-[14px] font-semibold text-zinc-950 hover:text-zinc-600`}
+                key={label}
+                href={href}
+                className="text-[14px] font-semibold text-zinc-950 hover:text-zinc-600"
               >
-                {route.label}
+                {label}
               </a>
             ))}
           </nav>
